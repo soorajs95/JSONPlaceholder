20128cd12f366df965dcf317c11a459483baac05
Sooraj <soorajs95@gmail.com>
2020-08-11 16:45:03 +0200

    Creating initial project structure

---
+++ src/test/java/configuration/RunCucumberTest.java
@@ -0,0 +1,11 @@
+package configuration;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty"})
+public class RunCucumberTest {
+
+}
