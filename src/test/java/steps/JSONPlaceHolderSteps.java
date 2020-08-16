b6b212bc34c1ac32ae1843d368ee4dda257a19f9
Sooraj <soorajs95@gmail.com>
2020-08-16 17:53:47 +0200

    Making code more readable

--- src/test/java/steps/JSONPlaceHolderSteps.java
+++ src/test/java/steps/JSONPlaceHolderSteps.java
@@ -4,15 +4,13 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.restassured.response.Response;
-import org.apache.commons.validator.routines.EmailValidator;
 import utilities.Utils;
 
-import javax.net.ssl.HttpsURLConnection;
+import java.net.HttpURLConnection;
 import java.util.ArrayList;
 
-import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.not;
 
 public class JSONPlaceHolderSteps extends Utils {
 
@@ -22,32 +20,32 @@ public class JSONPlaceHolderSteps extends Utils {
     @Given("The username to query {} and get the userId")
     public void usernameToQueryUsernameAndGetTheUserId(String username) {
         Response userResponse = getRequestWithParameter(USERS, "username", username);
-        userResponse.then().log().ifError().assertThat().statusCode(HttpsURLConnection.HTTP_OK)
-                .assertThat().body(matchesJsonSchemaInClasspath("schemas/users.json"));
+        verifyResponseStatusCode(userResponse, HttpURLConnection.HTTP_OK);
+        validateJsonResponseSchema(userResponse, "users.json");
+        userResponse.then().assertThat().body("id", not(empty()));
         userId = userResponse.path("id");
-        assertThat("No userId found for user " + username, userId.size(), greaterThan(0));
     }
 
     @Then("Get all the postIds for the userId")
     public void getAllThePostIdsForTheUserId() {
         Response userPostsResponse = getRequestWithParameter(POSTS, "userId", userId);
-        userPostsResponse.then().log().ifError().assertThat().statusCode(HttpsURLConnection.HTTP_OK)
-                .assertThat().body(matchesJsonSchemaInClasspath("schemas/posts.json"));
+        verifyResponseStatusCode(userPostsResponse, HttpURLConnection.HTTP_OK);
+        validateJsonResponseSchema(userPostsResponse, "posts.json");
+        userPostsResponse.then().assertThat().body("id", not(empty()));
         postIds = userPostsResponse.path("id");
-        assertThat("No postIds found for userID " + userId, postIds.size(), greaterThan(0));
     }
 
     @And("Get all the comments for the postIds")
     public void getAllTheCommentsForThePostIds() {
         userPostCommentsResponse = getRequestWithParameter(COMMENTS, "postId", postIds);
-        userPostCommentsResponse.then().log().ifError().assertThat().statusCode(HttpsURLConnection.HTTP_OK)
-                .assertThat().body(matchesJsonSchemaInClasspath("schemas/comments.json"));
+        verifyResponseStatusCode(userPostCommentsResponse, HttpURLConnection.HTTP_OK);
+        validateJsonResponseSchema(userPostCommentsResponse, "comments.json");
     }
 
     @Then("Validate email format in each comment")
     public void validateEmailFormatInEachComment() {
         ArrayList<String> emailIds = userPostCommentsResponse.path("email");
-        emailIds.forEach(email -> assertThat("Email not valid: " + email, EmailValidator.getInstance().isValid(email), is(equalTo(true))));
+        validateEmailFormat(emailIds);
     }
 
 }
