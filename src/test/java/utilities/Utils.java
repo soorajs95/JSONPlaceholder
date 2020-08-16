d261bbc4e2cb129649ef9541318e3cae49cd4abd
Sooraj <soorajs95@gmail.com>
2020-08-16 17:53:18 +0200

    Making code more readable

--- src/test/java/utilities/Utils.java
+++ src/test/java/utilities/Utils.java
@@ -1,10 +1,14 @@
 package utilities;
 
 import io.restassured.response.Response;
+import org.apache.commons.validator.routines.EmailValidator;
 
 import java.util.ArrayList;
 
-import static io.restassured.RestAssured.with;
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 public class Utils {
 
@@ -14,11 +18,28 @@ public class Utils {
     public static String COMMENTS = "/comments";
 
     public static Response getRequestWithParameter(String URI, String parameterKey, String parameterValue) {
-        return with().params(parameterKey, parameterValue).given().get(JSON_PLACEHOLDER_HOST + URI);
+        return given()
+                .queryParam(parameterKey, parameterValue)
+                .get(JSON_PLACEHOLDER_HOST + URI);
     }
 
     public static Response getRequestWithParameter(String URI, String parameterKey, ArrayList<Integer> parameterValue) {
-        return with().params(parameterKey, parameterValue).given().get(JSON_PLACEHOLDER_HOST + URI);
+        return given()
+                .queryParam(parameterKey, parameterValue)
+                .get(JSON_PLACEHOLDER_HOST + URI);
+    }
+
+    public static void verifyResponseStatusCode(Response response, int statusCode) {
+        response.then().assertThat().statusCode(statusCode);
+    }
+
+    public static void validateJsonResponseSchema(Response response, String jsonFileName) {
+        response.then().assertThat().body(matchesJsonSchemaInClasspath("schemas/" + jsonFileName));
+    }
+
+    public static void validateEmailFormat(ArrayList<String> emails) {
+        EmailValidator emailValidator = EmailValidator.getInstance();
+        emails.forEach(email -> assertThat("Email not valid: " + email, emailValidator.isValid(email), is(true)));
     }
 
 }
